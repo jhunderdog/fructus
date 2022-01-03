963b43b7e96af821ebfe5502670dd90a5b731e06
jhunderdog <33382water@gmail.com>
2022-01-03 22:18:08 +0900

    first

---
+++ Students/Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by jhunderdog on 2022/01/03.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
